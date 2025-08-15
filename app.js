0adae0cadd9fcd27cf49fd8a2c0a2c2413dfe55b
Alfaiz Sheikh <alfaizhsheikh1054@gmail.com>
2025-08-15 21:50:37 +0530

    Logic added and project completed

---
+++ app.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector(".clock");
+
+setInterval(() => {
+    let date = new Date();
+    clock.innerText = date.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true
+    });
+}, 1000);
